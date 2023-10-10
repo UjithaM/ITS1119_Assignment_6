f11836fd2623629aa7e8d705021a33a3f1a03230
uji100 <ujithamigara@gmail.com>
2023-10-10 08:53:17 +0530

    update add home manage links

--- assets/script/script.js
+++ assets/script/script.js
@@ -18,6 +18,13 @@ $('#navigation-bar>li').eq(1).on('click', () => {
     $('#order').css('display','none');
     $('#place_order').css('display','none');
 })
+$('#link-customers').on('click', () => {
+    $('#customer').css('display','block');
+    $('#home').css('display','none');
+    $('#item').css('display','none');
+    $('#order').css('display','none');
+    $('#place_order').css('display','none');
+})
 
 $('#navigation-bar>li').eq(2).on('click', () => {
     $('#item').css('display','block');
@@ -27,7 +34,23 @@ $('#navigation-bar>li').eq(2).on('click', () => {
     $('#place_order').css('display','none');
 })
 
+$('#link-items').on('click', () => {
+    $('#item').css('display','block');
+    $('#home').css('display','none');
+    $('#customer').css('display','none');
+    $('#order').css('display','none');
+    $('#place_order').css('display','none');
+})
+
 $('#navigation-bar>li').eq(3).on('click', () => {
+    $('#order').css('display','block');
+    $('#home').css('display','none');
+    $('#item').css('display','none');
+    $('#customer').css('display','none');
+    $('#place_order').css('display','none');
+})
+
+$('#link-orders').on('click', () => {
     $('#order').css('display','block');
     $('#home').css('display','none');
     $('#item').css('display','none');
